ea5fe4dd5807a37ca95d14e0103283806252255b
selimdilsad <selimdilsadercan@gmail.com>
2024-03-06 22:25:41 +0300

    feat(app): added folder-based data system

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <dirent.h>
 
 using namespace std;
 
@@ -10,61 +11,90 @@ bool startsWith(const std::string& str, const std::string& prefix) {
 }
 
 int main(){
-  fstream dataFile;
-  dataFile.open("data.txt", ios::in);
+//   fstream dataFile;
+//   dataFile.open("data.txt", ios::in);
 
-  string line;
-  int skillCategoryId = 0;
-  int skillId = 0;
-  int skillTopicId = 0;
-  int microSkillId = 0;
+//   string line;
+//   int skillCategoryId = 0;
+//   int skillId = 0;
+//   int skillTopicId = 0;
+//   int microSkillId = 0;
 
-  ofstream skillCategories;
-  skillCategories.open("export/1skillCategories.csv");
-  skillCategories << "id;name" << endl;
+//   ofstream skillCategories;
+//   skillCategories.open("export/1skillCategories.csv");
+//   skillCategories << "id;name" << endl;
 
-  ofstream skills;
-  skills.open("export/2skills.csv");
-  skills << "id;title;progress;imageUrl;skillCategoryId" << endl;
+//   ofstream skills;
+//   skills.open("export/2skills.csv");
+//   skills << "id;title;progress;imageUrl;skillCategoryId" << endl;
 
-  ofstream skilltopics;
-  skilltopics.open("export/3skilltopics.csv");
-  skilltopics << "id;name;skillId" << endl;
+//   ofstream skilltopics;
+//   skilltopics.open("export/3skilltopics.csv");
+//   skilltopics << "id;name;skillId" << endl;
 
-  ofstream microskills;
-  microskills.open("export/4microskills.csv");
-  microskills << "id;name;skillTopicId" << endl;
+//   ofstream microskills;
+//   microskills.open("export/4microskills.csv");
+//   microskills << "id;name;skillTopicId" << endl;
 
-  while(getline(dataFile, line)) {
-    if(line == "" || line == "----") {
-      continue;
+    string folderPath = "data";
+    DIR *dir;
+    struct dirent *entry;
+
+    if ((dir = opendir(folderPath.c_str())) != nullptr) {
+        while ((entry = readdir(dir)) != nullptr) {
+            string filename = entry->d_name;
+            if (filename != "." && filename != "..") {
+                ifstream file(folderPath + "/" + filename);
+                if (file.is_open()) {
+                    string line;
+                    while (getline(file, line)) {
+                        cout << line << endl;
+                    }
+                    file.close();
+                } else {
+                    cerr << "Unable to open file: " << folderPath + "/" + filename << endl;
+                }
+            }
+        }
+        closedir(dir);
+    } 
+    else {
+        cerr << "Unable to open directory: " << folderPath << endl;
+        return 1;
     }
-    else if(startsWith(line, "----")) { 
-      skillCategoryId++;
-      skillCategories << skillCategoryId << ";" << line.substr(5) << endl;
-    }
-    else if(startsWith(line, "--")) { 
-      skillId++;
-      skills << skillId << ";" << line.substr(3);
-      getline(dataFile, line);
-      skills << ";" << line;
-      getline(dataFile, line);
-      skills << ";" << line;
-      skills << ";" << skillCategoryId << endl;
-    }
-    else if(startsWith(line, "-")) { 
-      skillTopicId++;
-      skilltopics << skillTopicId << ";" << line.substr(2) << ";" << skillId << endl;
-    }
-    else { 
-      microSkillId++;
-      microskills << microSkillId << ";" << line << ";" << skillTopicId << endl;
-    }
-  }
 
-  skills.close();
-  skilltopics.close();
-  microskills.close();
+
+
+  // while(getline(dataFile, line)) {
+  //   if(line == "" || line == "----") {
+  //     continue;
+  //   }
+  //   else if(startsWith(line, "----")) { 
+  //     skillCategoryId++;
+  //     skillCategories << skillCategoryId << ";" << line.substr(5) << endl;
+  //   }
+  //   else if(startsWith(line, "--")) { 
+  //     skillId++;
+  //     skills << skillId << ";" << line.substr(3);
+  //     getline(dataFile, line);
+  //     skills << ";" << line;
+  //     getline(dataFile, line);
+  //     skills << ";" << line;
+  //     skills << ";" << skillCategoryId << endl;
+  //   }
+  //   else if(startsWith(line, "-")) { 
+  //     skillTopicId++;
+  //     skilltopics << skillTopicId << ";" << line.substr(2) << ";" << skillId << endl;
+  //   }
+  //   else { 
+  //     microSkillId++;
+  //     microskills << microSkillId << ";" << line << ";" << skillTopicId << endl;
+  //   }
+  // }
+
+//   skills.close();
+//   skilltopics.close();
+//   microskills.close();
 
   return 0;
 }
